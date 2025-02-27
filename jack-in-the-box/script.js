8c49387b9a4c4903ee628ecd1844e13c76826076
Snaarze <panlasiguijeremyglenn49@gmail.com>
2025-02-27 17:15:40 +0800

    fixed multiple timeout

--- jack-in-the-box/script.js
+++ jack-in-the-box/script.js
@@ -2,15 +2,20 @@ const container = document.querySelector(".container");
 const img = document.querySelector(".img-box");
 const btn = document.querySelector("button");
 
+let timeoutID = "";
+
 // change image depending on the state
 function changeImage() {
   const setNewImg = img.getAttribute("data-open-src");
   const closeNewImg = img.getAttribute("data-closed-src");
 
-  if (img.getAttribute("src") === setNewImg) {
+  if (img.getAttribute("src") === setNewImg && typeof timeoutID === "number") {
     // set the img src to close img, open text
     img.setAttribute("src", closeNewImg);
     btn.textContent = "Open";
+    // clear the timeout for if button is clicked again
+    // read this somewhere mdn documentation
+    clearTimeout(timeoutID);
     return;
   }
 
@@ -19,8 +24,7 @@ function changeImage() {
   btn.textContent = "Close";
 
   //   automatically closed after 3 seconds
-  setTimeout(() => {
-    console.log("test");
+  timeoutID = setTimeout(() => {
     img.setAttribute("src", closeNewImg);
     btn.textContent = "Open";
   }, 3000);
